03e644f9cad1cb74e5f35bd0de414a25efea289a
KM127PL <km127pl2@gmail.com>
2020-09-11 19:23:26 +0200

    Added Linux/Mac support in VersionDownloader.jar

--- src/com/github/km127pl/MinecraftCLI/MinecraftCLI.java
+++ src/com/github/km127pl/MinecraftCLI/MinecraftCLI.java
@@ -9,6 +9,7 @@ public class MinecraftCLI {
 	static String email;
 	static String password;
 	static String version;
+	static boolean areTheySet = false;
 	
 	public static void main(String[] args) throws ParseException, IOException {	
 		for(int i = 0; i<args.length; i++) {
@@ -28,38 +29,53 @@ public class MinecraftCLI {
 				break;
 				
 				default:
-					argList();
+					if(args.length < 1)
+						argList();
 				break;
 			}
 			
 			// System.out.println("args[" + i + "]: " + args[i]);
 		}
+		if(!(email == null) && !(password == null) && !(version == null)) {
+			areTheySet = true;
+		}
 		startVersion(version, true);
 	}
 	
 	public static void argList() {
-		
+		Logger.log("Arguments:");
+		System.out.println("-ver [Version]");
+		System.out.println("-email [Email]");
+		System.out.println("-password [Password]");
+		System.out.println("Example:");
+		System.out.println("java -jar MC-CLI.jar -ver 1.8 -email support@google.com -password password1");
 	}
 	
 	public static void startVersion(String v, Boolean ignore) throws ParseException, IOException {
-		System.out.println(Auth.getAuthToken(email, password));
-		/*switch(v) {
-			case "1.8":
-			case "1.8.9":
-				// start 1.8-9
-				System.out.println("Downloading version: " + v);
-				VersionDownloader.downloadVer(v, null);
-			try {
-				Logger.log(Auth.getAuthToken(email, password));
-			} catch (ParseException | IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		if(areTheySet) {
+			System.out.println(Auth.getAuthToken(email, password));
+			switch(v) {
+				case "1.8":
+				case "1.8.9":
+					// start 1.8-9
+					System.out.println("Downloading version: " + v);
+					VersionDownloader.downloadVer(v, null);
+				try {
+					Logger.log(Auth.getAuthToken(email, password));
+				} catch (ParseException | IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				break;
+				
+				default:
+					System.out.println("Selected version does not exist!");
+				break;
 			}
-			break;
-			
-			default:
-				System.out.println("Selected version does not exist!");
-			break;
+		} else {
+			Logger.log("Email or Password not set! Exiting..");
+			System.exit(0);
 		}
-	*/}
+		
+	}
 }
--- src/com/github/km127pl/MinecraftCLI/VersionDownloader.java
+++ src/com/github/km127pl/MinecraftCLI/VersionDownloader.java
@@ -3,16 +3,19 @@ package com.github.km127pl.MinecraftCLI;
 import java.nio.file.Path;
 
 public class VersionDownloader {
+	
+	public static String APPDATA = System.getenv("APPDATA") + "\\.MCCLI";
+	
 	public static void downloadVer(String name, Path path) {
 		switch(name) {
 			case "1.8":
 				FileManager.download("https://launcher.mojang.com/v1/objects/3870888a6c3d349d3771a3e9d16c9bf5e076b908/client.jar", "1.8.9.jar");
 				Logger.log("Downloading 1.8.9 jar");
-				if(!(FileManager.doesItExist(true, "C:\\Users\\" + FileManager.getWindowsUser() + "\\AppData\\Roaming\\.MCCLI"))) {
+				if(!(FileManager.doesItExist(true,  APPDATA))) {
 					Logger.log("Creating directory .MCCLI");
-					FileManager.createDir("C:\\Users\\" + FileManager.getWindowsUser() + "\\AppData\\Roaming\\.MCCLI");
+					FileManager.createDir(APPDATA);
 				}
-				FileManager.moveFile("1.8.9.jar", "C:\\Users\\" + FileManager.getWindowsUser() + "\\AppData\\Roaming\\.MCCLI\\1.8.9.jar");
+				FileManager.moveFile("1.8.9.jar", APPDATA + "\\1.8.9.jar");
 			break;
 		}
 	}
